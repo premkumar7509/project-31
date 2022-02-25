e334dbc60562eed785416c86d5be4f93eb2e9be0
premkumar7509 <95176121+premkumar7509@users.noreply.github.com>
2022-02-25 07:11:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var canvas;
+
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+
+  engine = Engine.create();
+  world = engine.World
+
+//create a player object from the Player class.
+PlayerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+player = new player(285, playerBase.body.position.y - 153, 50, 180);
+ 
+
+
+  computerBase = new ComputerBase( width - 300, random(450, height - 300), 180, 150);
+  
+  computer = new Computer(width - 280, computerBase.body.position.y - 153, 50, 180);
+
+  
+}
+
+function draw() {
+  background(189);
+
+  Engine.update(engine);
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+ 
+  playerBase.display();
+//call the display() function for the player object.
+  playerBase.display();
+  Player.display();
++
+
+  computerBase.display();
+  computer.display();
+  
+
+}
